c86f7403f1e10465bfa4c6ccaa6762415b68163c
alexdonahoe95 <34562687+alexdonahoe95@users.noreply.github.com>
2018-11-04 03:54:49 -0600

    Update Course.py

--- Course.py
+++ Course.py
@@ -1,21 +1,45 @@
 
 class Course:
-    def __init__(self,cCode, cName):
+    instructor = None
+    TA = None
+    def __init__(self,cCode, cName,inst =None,T = None):
         self.classCode = cCode
         self.className = cName
+        self.instructor = inst
+        self.TA = T
+
     def setClassCode(self, id):
         self.classCode = id
+
     def setClassName(self,name):
         self.className = name
+
     def setInstructor(self,instruct):
         self.instructor = instruct
+
     def setTA(self, T):
         self.TA = T
+
     def getClassCode(self):
         return(self.classCode)
+
     def getClassName(self):
         return(self.className)
+
     def getInstructor(self):
-        return(self.instructor)
+        if self.instructor is None:
+            return ("There is no Instructor Assigned")
+        else:
+            return(self.instructor)
+
     def getTA(self):
-        return(self.TA)
+        if self.TA is None:
+            return ("There is no TA Assigned")
+        else:
+            return(self.TA)
+
+    def printInfo(self):
+        return "\nClass Code:" + str(self.getClassCode())\
+               + "\nClass Name: " + self.getClassName() \
+               + "\nProfessor: " + self.getInstructor()\
+               +"\nTeachers Assistant: " + self.getTA()
