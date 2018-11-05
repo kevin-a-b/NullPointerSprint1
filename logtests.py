49d0c96da15e4f10fb02d69d84f48a8b2bd814a6
twtrinh <twtrinh@uwm.edu>
2018-11-04 19:10:33 -0600

    Login and Logoff with tests

---
+++ logtests.py
@@ -0,0 +1,55 @@
+from main import Project
+import main
+import unittest
+
+console = Project()
+
+
+class TestLogin(unittest.TestCase):
+    def test_login1(self):  # No username and password entered, no user logged in. Should return error
+        self.assertEqual(console.command("login"), "Error")
+
+    def test_login2(self):  # only username, no user logged in. Should return error
+        self.assertEqual(console.command("login username"), "Error")
+
+    def test_login3(self):  # only password, no user logged in. Should return error
+        self.assertEqual(console.command("login password"), "Error")
+
+    def test_login4(self):  # both, no user logged in. Should pass
+        self.assertEqual(console.command("login username password"), "Login Success")
+
+    def test_login5(self):  # No username and password entered, user logged in. Should return error
+        main.set_user(None)  # Just setting it to something for testing
+        self.assertEqual(console.command("login"), "Error")
+
+    def test_login6(self):  # only username, user logged in. Should return error
+        self.assertEqual(console.command("login username"), "Error")
+
+    def test_login7(self):  # only password, user logged in. Should return error
+        self.assertEqual(console.command("login password"), "Error")
+
+    def test_login8(self):  # both, user logged in. Should return error
+        main.set_user(1)  # Just setting it to something for testing
+        self.assertEqual(console.command("login username password"), "Another User is logged in")
+
+    def test_login9(self):
+        self.assertEqual(console.command("login too any arguements"), "Error")
+
+    def test_logoff1(self):  # User logged in
+        main.set_user(1)  # Just setting it to something for testing
+        self.assertEqual(console.command("logout"), "Logout Success")
+
+    def test_logoff2(self):  # No one logged in
+        self.assertEqual(console.command("logout"), "Error")
+
+    def test_logoff3(self):  # Someone passes extra arguements
+        self.assertEqual(console.command("logout a b c"), "Error")
+
+    def test_loginLogoff(self):
+        main.set_user(None)  # Just setting it to something for testing
+        self.assertEqual(console.command("login username password"), "Login Success")
+        self.assertEqual(console.command("logout"), "Logout Success")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from Course import Course
+from memory import Memory
+
 classlist = []
 
-class main:
+cmd = {}
+# Command list for main class
 
+
+def set_user(user):  # Set current user
+    global current_user # set global so it can be accessed from other methods in this file
+    current_user = user
+
+
+def getuser():  # Get current user(for external files) PEP8 makes
+    return current_user
+
+
+class Project:
+    def __init__(self):
+        self.commands = cmd
+
+    def command(self, strcmd):
+        split = strcmd.split()
+        return self.commands[split[0]](split[1:])
 
     def createClass(self,cName):
         classlist.append(Course(classlist.__len__(), cName))
@@ -22,3 +42,34 @@ class main:
     def printAllClasses(self):
         for x in range(0,len(classlist)):
             print(classlist[x].printInfo())
+
+
+def login(user_pass):  # user_pass short for list contained username and password
+    user = current_user  # For some reason we can't directly use current_user but we can set stuff to it
+    if len(user_pass) != 2:  # If wrong amount of arguments
+        return "Error"
+    elif not (user is None):  # If a user is logged in already logged in
+        return "Another User is logged in"
+    else:
+        user = Memory.read_account(user_pass[0], user_pass[1])
+    if user is None:
+        return "Error"
+    set_user(user)
+    return "Login Success"
+
+
+cmd["login"] = login  # Add login command to command list
+
+
+def logout(extra):  # Log Off Command, extra is unnecessary arguments passed through
+    if len(extra) > 0:  # If any extra arguments
+        return "Error"
+    user = current_user
+    if user is None:  # We can't logoff if there is nobody to logoff
+        return "Error"
+    else:  # Set current user to none since no one is logged in
+        set_user(None)
+        return "Logout Success"
+
+
+cmd["logout"] = logout  # Add logoff command to command list
