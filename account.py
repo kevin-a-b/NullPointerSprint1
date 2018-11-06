d6fb7854e08d45d40089f7cd4e6fac716533ef28
97215 <972157524@qq.com>
2018-11-06 12:10:06 -0600

    merged account and test

--- account.py
+++ account.py
@@ -65,3 +65,56 @@ class Account:
         Memory.delete_account(stringList[0])
         return
 
+    def edit_account_username(self, new_username):
+        if self.username == new_username:
+            print("it is the current username")
+        elif new_username == None:
+            print("plz enter something")
+        else:
+            self.username = new_username
+            print("username updated")
+
+    def edit_account_password(self, new_password):
+        if self.password == new_password:
+            print("it is the current password")
+        elif new_username == None:
+            print("plz enter something")
+        else:
+            self.password = new_password
+            print("password updated")
+
+    def edit_account_name(self, new_name):
+        if self.name == new_name:
+            print("it is the current name")
+        elif new_username == None:
+            print("plz enter something")
+        else:
+            self.name = new_name
+            print("name updated")
+
+    def edit_account_address(self, new_address):
+        if self.address == new_address:
+            print("it is the current address")
+        elif new_username == None:
+            print("plz enter something")
+        else:
+            self.address = new_address
+            print("address updated")
+
+    def edit_account_email(self, new_email):
+        if self.mail == new_email:
+            print("it is the current email")
+        elif new_username == None:
+            print("plz enter something")
+        else:
+            self.email = new_email
+            print("email updated")
+
+    def edit_account_phonenumber(self, new_phonenumber):
+        if self.phonenumber == new_phonenumber:
+            print("it is the current phonenumber")
+        elif new_username == None:
+            print("plz enter something")
+        else:
+            self.phonenumber = new_phonenumber
+            print("phonenumber updated")
--- accountTester.py
+++ accountTester.py
@@ -1,7 +1,8 @@
 from account import Account
 import unittest
 
-
+user = Account('Snowman', '20181225', 'SnowJackal', 'backyard',
+               'merryXmas@winter.com', 414000111, 3)  # does flag number matters here?
 class TestAccountMethods(unittest.TestCase):
 
     def setUp(self):
@@ -47,6 +48,60 @@ class TestAccountMethods(unittest.TestCase):
     def test_deleteAccount_long_list(self):
         with self.assertRaises(Exception):
             Account.delete_account(self.superAccount, [0, 1, 2])
+
+    def test_username1(self):
+        self.assertEqual(user.edit_account_username("Snowman"), "it is the current username")
+
+    def test_username2(self):
+        self.assertEqual(user.edit_account_username("Fireman"), "username updated")
+
+    def test_username3(self):
+        self.assertEqual(user.edit_account_username(), "plz enter something")
+
+    def test_password1(self):
+        self.assertEqual(user.edit_account_password("20181225"), "it is the current password")
+
+    def test_password2(self):
+        self.assertEqual(user.edit_account_password("20181231"), "password updated")
+
+    def test_password3(self):
+        self.assertEqual(user.edit_account_password(), "plz enter something")
+
+    def test_name1(self):
+        self.assertEqual(user.edit_account_name("SnowJackal"), "it is the current name")
+
+    def test_name2(self):
+        self.assertEqual(user.edit_account_name("FireFly"), "name updated")
+
+    def test_name3(self):
+        self.assertEqual(user.edit_account_name(), "plz enter something")
+
+    def test_address1(self):
+        self.assertEqual(user.edit_account_address("backyard"), "it is the current address")
+
+    def test_address2(self):
+        self.assertEqual(user.edit_account_address("street"), "address updated")
+
+    def test_address3(self):
+        self.assertEqual(user.edit_account_address(), "plz enter something")
+
+    def test_email1(self):
+        self.assertEqual(user.edit_account_email("merryXmas@winter.com"), "it is the current email")
+
+    def test_email2(self):
+        self.assertEqual(user.edit_account_email("seeyounextyear@spring.com"), "email updated")
+
+    def test_email3(self):
+        self.assertEqual(user.edit_account_email(), "plz enter something")
+
+    def test_phonenumber1(self):
+        self.assertEqual(user.edit_account_phonenumber(414000111), "it is the current phonenumber")
+
+    def test_phonenumber2(self):
+        self.assertEqual(user.edit_account_phonenumber(414222333), "phonenumber updated")
+
+    def test_phonenumber3(self):
+        self.assertEqual(user.edit_account_phonenumber(), "plz enter something")
 
 
 if __name__ == '__main__':
